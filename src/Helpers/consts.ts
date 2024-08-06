3e8cdb2baf9838e967f84c575681cbeeddaad7cc
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-06 12:06:13 -0300

    + player speed and artificial delay

--- src/Helpers/consts.ts
+++ src/Helpers/consts.ts
@@ -8,7 +8,8 @@
 /// * `PLAYER_COLOR` - The color used to draw players
 
 export const TPS = 32;
-export const PLAYER_SPEED = 1;
+export const PLAYER_SPEED = 5;
 export const PLAYER_RADIUS = 10;
 export const PLAYER_COLOR = '#FF0000';
 export const PID = Math.floor(Math.random() * (2 ** 16));
+export const ARTIFICIAL_DELAY = 100;
