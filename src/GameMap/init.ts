aa37565489fb7d1c9804c1e587c9148821c5ff3d
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-07 12:06:05 -0300

    fix merge error

--- src/GameMap/init.ts
+++ src/GameMap/init.ts
@@ -19,13 +19,6 @@ export function init(): GameMap {
     { kind: 'Wall', position: { x: 100, y: 200 }, width: width - 400, height: 10 },
     { kind: 'Platform', position: { x: 100, y: height - 100 }, width: 100, height: 20 },
   ];
-    { kind: 'Wall', position: { x: 100, y: 200 }, width: width - 400, height: 10 },
-    //{ kind: 'Wall', position: { x: 0, y: height - 10 }, width: width, height: 10 },
-    //{ kind: 'Wall', position: { x: 0, y: 0 }, width: 10, height: height },
-    //{ kind: 'Wall', position: { x: width - 10, y: 0 }, width: 10, height: height },
-    { kind: 'Platform', position: { x: 100, y: height - 100 }, width: 100, height: 20 },
-    //{ kind: 'Platform', position: { x: width - 300, y: height - 200 }, width: 200, height: 20 },
-  ];
 
   return {
     width: width,
