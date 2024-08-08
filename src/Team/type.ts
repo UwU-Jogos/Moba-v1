796dad2b26cc3774d5c13c1a91975d0283c55ea8
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-08 18:40:45 -0300

    team type

---
+++ src/Team/type.ts
@@ -0,0 +1,15 @@
+/// Represents the teams in the game.
+///
+/// # Description
+///
+/// An enum defining the possible teams a player can belong to.
+///
+/// # Values
+///
+/// * `TEAM_RED` - Represents the red team
+/// * `TEAM_BLUE` - Represents the blue team
+
+export enum TeamType {
+  TEAM_RED,
+  TEAM_BLUE
+}
