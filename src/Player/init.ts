d489ef391e041f0995ea0890b274506cc78e7664
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-09 09:34:21 -0300

    player has to be a character

--- src/Player/init.ts
+++ src/Player/init.ts
@@ -18,26 +18,24 @@ import { TeamType } from '../Team/type';
 import { PLAYER_INITIAL_LIFE } from '../Helpers/consts';
 import { basic } from '../Skill/basic';
 import { PLAYER_RADIUS } from '../Helpers/consts';
+import { CharacterType } from '../Character/type';
 import { seconds_to_ticks } from '../Helpers/seconds_to_ticks';
+import { create_character } from '../Character/create_character';
 
 var next_team : TeamType = TeamType.TEAM_RED;
 
-export function init(id: UID, name: Name, pos: V2): Player {
-
-
+export function init(id: UID, name: Name, pos: V2, character_type: CharacterType): Player {
+  let character = create_character(character_type);
   let initial_player : Player = {
     id,
     name,
     pos,
     target_pos: pos,
-    skills: {
-      'Q': { id: 'skill1', type: 'melee', cooldown: seconds_to_ticks(1), duration: 1, range: PLAYER_RADIUS * 2 },
-      'W': { id: 'skill2', type: 'target', cooldown: seconds_to_ticks(1), duration: 1, range: PLAYER_RADIUS * 2 },
-      'E': { id: 'skill3', type: 'action', cooldown: seconds_to_ticks(0.25), duration: 1, range: 200 },
-    },
+    skills: character.skills,
     active_skills: {},
     life: PLAYER_INITIAL_LIFE,
-    team: next_team
+    team: next_team,
+    character: character_type
   };
   next_team = (next_team == TeamType.TEAM_RED) ? TeamType.TEAM_BLUE : TeamType.TEAM_RED;
 
