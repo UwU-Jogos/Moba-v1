c5d12e1d69047524f92e672056088c28faeca8c1
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-09 15:05:55 -0300

    effects on character

--- src/Character/_.ts
+++ src/Character/_.ts
@@ -2,25 +2,36 @@
 ///
 /// # Description
 ///
-/// A character with a name, type, and skills.
+/// A character with a name, type, skills, and effects.
 ///
-/// # Input
+/// # Fields
 ///
 /// * `name` - The name of the character
 /// * `type` - The type of the character (CharacterType)
+/// * `skills` - A map of keys to basic skills
+/// * `effects` - An array of effects applied to the character
 ///
-/// # Output
+/// # Type Definition
 ///
-/// A Character object
+/// A Character object with the following structure:
+/// ```
+/// {
+///   name: string;
+///   type: CharacterType;
+///   skills: { [key: Key]: basic };
+///   effects: Effect[];
+/// }
+/// ```
 
 import { CharacterType } from './type';
 import { Key } from '../Key/_';
 import { DEFAULT_RANGE } from '../Helpers/consts';
 import { basic } from '../Skill/basic';
+import { Effect } from '../Effect/_';
 
 export type Character = {
   name: string;
   type: CharacterType;
   skills: { [key: Key]: basic };
+  effects: Effect[];
 };
-
