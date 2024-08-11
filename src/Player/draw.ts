e01c9bc5d5879ec1bcf4d6b3ab44ee4703214184
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-11 10:12:52 -0300

    collision and player drawing

--- src/Player/draw.ts
+++ src/Player/draw.ts
@@ -28,7 +28,7 @@ export function draw(ctx: CanvasRenderingContext2D, player: Player): void {
   if (player.stats.destroyed_orbs >= 5) {
     team_color = 'rgb(0, 0, 0)'; // Black
   } else {
-    const base_color = player.team === TeamType.TEAM_RED ? [255, 100, 100] : [100, 100, 255];
+    const base_color = player.team === TeamType.TEAM_RED ? [255, 100, 100] : [150, 150, 255];
     const darkness = Math.min(player.stats.destroyed_orbs / 5, 1);
     const r = Math.floor(base_color[0] * (1 - darkness));
     const g = Math.floor(base_color[1] * (1 - darkness));
@@ -47,7 +47,7 @@ export function draw(ctx: CanvasRenderingContext2D, player: Player): void {
   const life_bar_height = 5; 
   const life_bar_x = player.pos.x - life_bar_width / 2;
   const life_bar_y = player.pos.y + 20; 
-  const life_percentage = (player.life / PLAYER_INITIAL_LIFE);
+  const life_percentage = (player.life / player.stats.max_life);
 
   ctx.fillStyle = 'red';
   ctx.fillRect(life_bar_x, life_bar_y, life_bar_width, life_bar_height);
