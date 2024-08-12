d4aec6e558a2bf2a9b219e9b12d3c597dd50ab76
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-12 16:09:15 -0300

    refac and check pentagon

--- src/Projectile/check_player_collision.ts
+++ src/Projectile/check_player_collision.ts
@@ -5,28 +5,29 @@ import { take_damage } from '../Player/take_damage';
 import { UID } from '../UID/_';
 import { Projectile } from './_';
 import { create_character } from '../Character/create_character';
+import { CharacterType } from '../Character/type';
 
 export function check_player_collision(projectile: Projectile, player: Player, player_id: UID): [Player, Projectile] {
   if (projectile.owner_id === player_id) {
     return [player, projectile];
   }
 
-  const dist = distance(projectile.pos, player.pos);
-
-  if (dist <= PLAYER_RADIUS) {
-    const character = create_character(player.character);
-    const immune_effect = player.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
-    const char_immune_effect = character.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
-
-    const useless_projectile: Projectile = {
+  const useless_projectile: Projectile = {
       ...projectile,
       remaining_distance: 0,
       remaining_duration: 0,
-      speed: 0,
-      damage: 0
     };
 
-    if (immune_effect || char_immune_effect) {
+
+  const dist = distance(projectile.pos, player.pos);
+
+  if (dist <= PLAYER_RADIUS) {
+    const character_obj = create_character(player.character);
+    const immune_effect = player.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
+    const char_immune_effect = character_obj.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
+    const pentagon_player = player.character == CharacterType.PENTAGON;
+    
+    if (immune_effect || char_immune_effect || pentagon_player) {
       return [player, useless_projectile];
     } else {
       const damage_to_take = projectile.damage * player.stats.damage_multiplier;
