e1ae87a8c2f35bf5afd63b30b8357356e9c1961c
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-12 12:24:28 -0300

    activate multiple skills at the same time

--- src/Skill/activate.ts
+++ src/Skill/activate.ts
@@ -4,6 +4,7 @@ import { create as create_projectile } from "../Projectile/create";
 import { UID } from "../UID/_";
 import { Damage } from "../Damage/_";
 import { V2 } from "../V2/_";
+import { create_character } from '../Character/create_character';
 
 // Update the activateSkill function
 export function activate(gs: GameState, player_id: UID, skill_key: Key, target_pos: V2, damage: Damage): GameState {
@@ -15,13 +16,22 @@ export function activate(gs: GameState, player_id: UID, skill_key: Key, target_p
   
   const current_tick = gs.tick;
   if (player.active_skills[skill.id] && current_tick < player.active_skills[skill.id]) {
-    console.log("Skill is still on cooldown", player.active_skills[skill.id]);
     return gs;
   }
   
-  // Activate the skill
-  const [id, new_projectile] = create_projectile(skill, player_id, player.pos, target_pos, damage);
-  const new_projectile_system = gs.projectile_system.set(id, new_projectile);
+  // Get the character and check for MultipleShot effect
+  const character = create_character(player.character);
+  const multiple_shot = character.effects.find(effect => effect.$ === 'MultipleShot');
+  const shots_number = multiple_shot ? multiple_shot.shots_number : 1;
+
+  let new_projectile_system = gs.projectile_system;
+
+  // Create multiple projectiles if MultipleShot effect exists
+  for (let i = 0; i < shots_number; i++) {
+    const [id, new_projectile] = create_projectile(skill, player_id, player.pos, target_pos, damage);
+    const unique_id = `${id}-${i}`; // Add an index to make each projectile ID unique
+    new_projectile_system = new_projectile_system.set(unique_id, new_projectile);
+  }
   
   // Update the skill cooldown
   const new_active_skills = { ...player.active_skills, [skill.id]: current_tick + skill.cooldown };
