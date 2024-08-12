6522e1096cac99e938795721b40352925160ad81
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-12 12:24:13 -0300

    handle projectile collision effects

--- src/Projectile/check_player_collision.ts
+++ src/Projectile/check_player_collision.ts
@@ -4,6 +4,7 @@ import { Player } from '../Player/_';
 import { take_damage } from '../Player/take_damage';
 import { UID } from '../UID/_';
 import { Projectile } from './_';
+import { create_character } from '../Character/create_character';
 
 export function check_player_collision(projectile: Projectile, player: Player, player_id: UID): [Player, Projectile] {
   if (projectile.owner_id === player_id) {
@@ -13,7 +14,10 @@ export function check_player_collision(projectile: Projectile, player: Player, p
   const dist = distance(projectile.pos, player.pos);
 
   if (dist <= PLAYER_RADIUS) {
+    const character = create_character(player.character);
     const immune_effect = player.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
+    const char_immune_effect = character.effects.find(effect => effect.$ === 'Immune' && effect.active > 0);
+
     const useless_projectile: Projectile = {
       ...projectile,
       remaining_distance: 0,
@@ -22,7 +26,7 @@ export function check_player_collision(projectile: Projectile, player: Player, p
       damage: 0
     };
 
-    if (immune_effect) {
+    if (immune_effect || char_immune_effect) {
       return [player, useless_projectile];
     } else {
       const damage_to_take = projectile.damage * player.stats.damage_multiplier;
