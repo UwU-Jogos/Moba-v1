eb3b6426912e5a11aeb99df29030fd447db0c199
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-13 14:34:26 -0300

    smaller tick functions for projectile

--- src/Projectile/check_game_object_collision.ts
+++ src/Projectile/check_game_object_collision.ts
@@ -16,10 +16,6 @@ import { match as match_game_object } from '../GameMap/GameObject/match';
 import { distance } from '../Helpers/distance';
 
 export function check_game_object_collision(projectile: Projectile, game_object: GameObject): [GameObject, Projectile] {
-  if (projectile.effects.some(effect => effect.$ === 'ShotThroughWall')) {
-    return [game_object, projectile];
-  }
-
   return match_game_object(game_object, {
     Wall: (position, width, height) => check_rectangle_collision(projectile, game_object, position, width, height),
     Platform: (position, width, height) => check_rectangle_collision(projectile, game_object, position, width, height),
@@ -48,6 +44,10 @@ export function check_game_object_collision(projectile: Projectile, game_object:
 }
 
 function check_rectangle_collision(projectile: Projectile, game_object: GameObject, position: V2, width: number, height: number): [GameObject, Projectile] {
+  if (projectile.effects.some(effect => effect.$ === 'ShotThroughWall')) {
+    return [game_object, projectile];
+  }
+
   const left = position.x;
   const right = position.x + width;
   const top = position.y;
@@ -70,6 +70,10 @@ function check_rectangle_collision(projectile: Projectile, game_object: GameObje
 }
 
 function check_line_wall_collision(projectile: Projectile, game_object: GameObject, ini: V2, end: V2): [GameObject, Projectile] {
+  if (projectile.effects.some(effect => effect.$ === 'ShotThroughWall')) {
+    return [game_object, projectile];
+  }
+
   const dx = end.x - ini.x;
   const dy = end.y - ini.y;
   const length = Math.sqrt(dx * dx + dy * dy);
