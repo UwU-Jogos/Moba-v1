958d046961f17945f49e917d0fc5701685527839
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-19 07:20:43 -0300

    default cds

--- src/Helpers/consts.ts
+++ src/Helpers/consts.ts
@@ -6,11 +6,20 @@
 /// * `PLAYER_SPEED` - The movement speed of players
 /// * `PLAYER_RADIUS` - The radius of the player's circular representation
 /// * `PLAYER_COLOR` - A randomly generated color used to draw players
+/// * `PLAYER_INITIAL_LIFE` - The initial life points of a player
 /// * `PID` - A randomly generated player ID
 /// * `ARTIFICIAL_DELAY` - An artificial delay added to actions (in milliseconds)
 /// * `WALL_COLOR` - The color used for walls
 /// * `PLATFORM_COLOR` - The color used for platforms
-/// * `RESPAWN_COLOR` - The color used for respawn area
+/// * `PLAYERS_LIMIT` - The maximum number of players allowed in the game
+/// * `TIME_TO_START_GAME` - The countdown time before the game starts (in seconds)
+/// * `DEFAULT_RANGE` - The default range for player actions
+/// * `RESPAWN_AREA_COLOR` - The color used for respawn areas
+/// * `ORB_COLOR` - The color used for orbs
+/// * `GAME_TIME` - The total duration of the game (in seconds)
+/// * `DEFAULT_ATTACK_DAMAGE` - The default damage value for attacks
+/// * `DEFAULT_ATTACK_COOLDOWN` - The default cooldown time for attacks
+/// * `DEFAULT_HEAL_COOLDOWN` - The default cooldown time for healing abilities (in ticks)
 
 export const TPS = 32;
 export const PLAYER_SPEED = 1.5;
@@ -28,3 +37,5 @@ export const RESPAWN_AREA_COLOR = 'green';
 export const ORB_COLOR = 'blue';
 export const GAME_TIME = 60 * 10;
 export const DEFAULT_ATTACK_DAMAGE = 10;
+export const DEFAULT_ATTACK_COOLDOWN = 5;
+export const DEFAULT_HEAL_COOLDOWN = 5 * TPS;
