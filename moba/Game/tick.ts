ebfa86acae4e960389c9c20a6793fa70dae71742
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-08-23 10:04:50 -0300

    update tick

--- moba/Game/tick.ts
+++ moba/Game/tick.ts
@@ -7,14 +7,15 @@ import { check_collision } from '../Body/check_collision';
 // Updates the game state for a single tick.
 // - gs: The current game state
 // = The updated GameState after processing a single tick
-
 export function tick(gs: GameState): GameState {
   let updated_gs = gs;
 
   gs.game_map.bodies.forEach((body : Body) => {
-    updated_gs = body.tick(gs);
-    updated_gs = check_collision(updated_gs);
+    updated_gs = body.tick(updated_gs);
   });
 
-  return updated_gs;
+  return {
+    ...updated_gs,
+    tick: updated_gs.tick + 1  
+  };
 }
